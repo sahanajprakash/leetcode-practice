1b204fa7e2bcb84aa8e407136873678eb60667f4
Sahana J Prakash <1999sahana@gmail.com>
2024-02-05 02:22:32 +0530

    Time: 54 ms (49.34%) | Memory: 48.9 MB (8.24%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -5,13 +5,8 @@
 var numberOfSteps = function(num) {
     var step = 0
     while(num!=0){
-        if(num%2==0){
-            num=num/2
-            step++
-        } else {
-            num--
-            step++
-        }
+        num % 2 === 0 ? num /= 2 : num--;
+        step++;
     }
     return step
 };
