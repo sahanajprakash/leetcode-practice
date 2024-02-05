2e72dc96f42d79be1d04933bc1894d9ce69cae7a
Sahana J Prakash <1999sahana@gmail.com>
2024-02-05 18:33:24 +0530

    Time: 70 ms (13.65%) | Memory: 51.5 MB (6.01%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count = 0;
+  for (let num of nums) {
+    let digit = 0;
+
+    while (num > 0) {
+      digit++;
+      num = parseInt(num / 10);
+    }
+
+    if (digit % 2 === 0) count++;
+  }
+
+  return count;
+};
