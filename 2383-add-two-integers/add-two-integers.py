1953e8e5831b142ec5ee83f074e9653b80551c9b
Sahana J Prakash <1999sahana@gmail.com>
2025-01-25 19:00:04 -0500

    Time: 3 ms (3.01%) | Memory: 18.3 MB (17.38%) - LeetSync

--- 2383-add-two-integers/add-two-integers.py
+++ 2383-add-two-integers/add-two-integers.py
@@ -1,38 +1,3 @@
-class Int:
-    def __init__(self, value=0):
-        self._value = value
-
-    def int(self):
-        return self._value
-
-    def __eq__(self, other):
-        return self._value == other._value
-
-    def __ne__(self, other):
-        return self._value != other._value
-
-    def __lt__(self, other):
-        return self._value < other._value
-
-    def __le__(self, other):
-        return self._value <= other._value
-
-    def __gt__(self, other):
-        return self._value > other._value
-
-    def __ge__(self, other):
-        return self._value >= other._value
-
-    def __add__(self, other):
-        return Int(self._value + other._value)
-
-    def __str__(self):
-        return str(self._value)
-###########################################################
-# WRIRE  class  Int
-###########################################################
-
-
 class Solution:
     def sum(self, a: int, b: int) -> int:
         c = a + b
@@ -57,23 +22,160 @@ class Solution:
         assert(c == ans)
         for i in range(len(relation)):
           assert(relation[i] == mrelation[i])
-        return ans 
+        return ans
+
+
+########################################
+# WRITE CODE BELOW
+########################################
 class Exam:
-    def __init__(self, a: 'Int', b: 'Int', add: 'Int', relation: 'list of bool') -> 'None':
-        # Compute addition and update add directly
-        add._value = (a + b).int()
+    def __init__(self, a:'Int', b:'Int', add:'Int',relation:'list of bool')->'None':
+         # Use Int class logic to perform operations
+        result = a + b
+        add._a = result._a
+        add._positive = result._positive
 
-        # Compute relations based on Boolean logic
-        less_than = a < b
-        greater_than = a > b
-        equal_to = not less_than and not greater_than
+        # Populate relations
+        relation.append(a < b)
+        relation.append(a <= b)
+        relation.append(a > b)
+        relation.append(a >= b)
+        relation.append(a == b)
+        relation.append(a != b)
+############################################################
+# Int.py
+# Implements Int object
+# Author: Jagadeesh Vasudevamurthy
+# Copyright: Jagadeesh Vasudevamurthy 2025
+# added to git
+###########################################################
 
-        # Populate the relation list
-        relation.extend([
-            less_than,            # a < b
-            less_than or equal_to, # a <= b
-            greater_than,         # a > b
-            greater_than or equal_to, # a >= b
-            equal_to,             # a == b
-            not equal_to          # a != b
-        ])
+class Int:
+    def __init__(self, n: int = 0):
+        self._positive = n >= 0
+        self._a = self.build(abs(n))
+
+    def build(self, n: int) -> 'list of int':
+        return [int(x) for x in str(n)]
+
+    def int(self) -> int:
+        value = int("".join(map(str, self._a)))
+        return value if self._positive else -value
+
+    def __len__(self):
+        return len(self._a)
+
+    def __getitem__(self, index: int) -> int:
+        return self._a[index]
+
+    def __setitem__(self, index: int, value: int):
+        self._a[index] = value
+
+    def __str__(self) -> str:
+        sign = "" if self._positive else "-"
+        return sign + "".join(map(str, self._a))
+
+    def __eq__(self, other: 'Int') -> bool:
+        return self.int() == other.int()
+
+    def __lt__(self, other: 'Int') -> bool:
+        return self.int() < other.int()
+
+    def __le__(self, other: 'Int') -> bool:
+        return self.int() <= other.int()
+
+    def __gt__(self, other: 'Int') -> bool:
+        return self.int() > other.int()
+
+    def __ge__(self, other: 'Int') -> bool:
+        return self.int() >= other.int()
+
+    def __ne__(self, other: 'Int') -> bool:
+        return self.int() != other.int()
+
+    def __add__(self, other: 'Int') -> 'Int':
+        if self._positive == other._positive:
+            # Add absolute values
+            result = self._ripple_carry_add(self._a, other._a)
+            return Int(int("".join(map(str, result))) * (1 if self._positive else -1))
+        else:
+            # Handle subtraction of absolute values
+            if self._absolute_gte(self._a, other._a):  # self >= other
+                result = self._ripple_carry_sub(self._a, other._a)
+                return Int(int("".join(map(str, result))) * (1 if self._positive else -1))
+            else:  # self < other
+                result = self._ripple_carry_sub(other._a, self._a)
+                return Int(int("".join(map(str, result))) * (-1 if self._positive else 1))
+
+    def _absolute_gte(self, a: 'list of int', b: 'list of int') -> bool:
+        """
+        Helper method to determine if absolute value of `a` >= absolute value of `b`.
+        """
+        if len(a) > len(b):
+            return True
+        if len(a) < len(b):
+            return False
+        return a >= b  # Lexicographical comparison for equal lengths
+
+
+    def __sub__(self, other: 'Int') -> 'Int':
+        # Subtract by adding the negative
+        neg_other = Int(-other.int())
+        return self + neg_other
+
+    def _ripple_carry_add(self, a: 'list of int', b: 'list of int') -> 'list of int':
+        # Ensure `a` is the longer list
+        if len(a) < len(b):
+            a, b = b, a
+
+        result = []
+        carry = 0
+        i, j = len(a) - 1, len(b) - 1
+
+        # Add digits from the least significant to the most significant
+        while i >= 0 or j >= 0 or carry:
+            digit_a = a[i] if i >= 0 else 0
+            digit_b = b[j] if j >= 0 else 0
+            total = digit_a + digit_b + carry
+            result.append(total % 10)
+            carry = total // 10
+            i -= 1
+            j -= 1
+
+        return result[::-1]  # Reverse for the correct order
+
+    def _ripple_carry_sub(self, a: 'list of int', b: 'list of int') -> 'list of int':
+        # Subtract `b` from `a` assuming `a >= b`
+        result = []
+        borrow = 0
+        i, j = len(a) - 1, len(b) - 1
+
+        while i >= 0:
+            digit_a = a[i]
+            digit_b = b[j] if j >= 0 else 0
+            diff = digit_a - digit_b - borrow
+
+            if diff < 0:
+                diff += 10
+                borrow = 1
+            else:
+                borrow = 0
+
+            result.append(diff)
+            i -= 1
+            j -= 1
+
+        # Remove leading zeros
+        while len(result) > 1 and result[-1] == 0:
+            result.pop()
+
+        return result[::-1]  # Reverse for the correct order
+
+
+
+
+
+###########################################################
+# WRIRE  class  Int
+###########################################################
+
